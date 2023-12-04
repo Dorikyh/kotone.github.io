d284498253848e79890c7cff3ab50841ab95196e
Fabián Márquez <80162941+Dorikyh@users.noreply.github.com>
2023-12-04 01:17:41 -0500

    Fetch API

---
+++ script.js
@@ -0,0 +1,18 @@
+function updateStatistics() {
+    fetch('http://78.108.218.94:19132/api/stats')
+      .then(response => response.json())
+      .then(data => {
+        // Update values in the HTML
+        document.getElementById('servers').textContent = data.servers;
+        document.getElementById('users').textContent = data.users;
+        document.getElementById('commands').textContent = data.used_commands;
+        // You can add logic here to update the third value of commands used if provided in the JSON
+        // For example:
+        // document.getElementById('commands').textContent = data.commands;
+      })
+      .catch(error => console.error('Error fetching data from API:', error));
+  }
+  
+  // Call the function when the page loads to update the statistics
+  updateStatistics();
+  
