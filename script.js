0a92f82db7b35b45ad3b08cdd909a2a53c68bb0c
Fabián Márquez <80162941+Dorikyh@users.noreply.github.com>
2023-12-31 14:21:00 -0500

    Format numbers with commas using toLocaleString()

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function updateStatistics() {
-    fetch('https://api.kotone.tech/stats')
-      .then(response => response.json())
-      .then(data => {
-        // Update values in the HTML
-        document.getElementById('servers').textContent = data.servers;
-        document.getElementById('users').textContent = data.users;
-        document.getElementById('commands').textContent = data.used_commands;
-        // You can add logic here to update the third value of commands used if provided in the JSON
-        // For example:
-        // document.getElementById('commands').textContent = data.commands;
-      })
-      .catch(error => console.error('Error fetching data from API:', error));
-  }
-  
-  // Call the function when the page loads to update the statistics
-  updateStatistics();
-  
+  fetch('https://api.kotone.tech/stats')
+    .then(response => response.json())
+    .then(data => {
+      // Format numbers with commas using toLocaleString()
+      const servers = data.servers.toLocaleString();
+      const users = data.users.toLocaleString();
+      const usedCommands = data.used_commands.toLocaleString();
+
+      // Update values in the HTML with formatted numbers
+      document.getElementById('servers').textContent = servers;
+      document.getElementById('users').textContent = users;
+      document.getElementById('commands').textContent = usedCommands;
+      
+      // You can add logic here to update the third value of commands used if provided in the JSON
+      // For example:
+      // const commands = data.commands.toLocaleString();
+      // document.getElementById('commands').textContent = commands;
+    })
+    .catch(error => console.error('Error fetching data from API:', error));
+}
+
+// Call the function when the page loads to update the statistics
+updateStatistics();
